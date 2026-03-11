963523458c69bee9296e04194a84f0c5468f7c24
maycon <maynions@gmail.com>
2026-03-11 10:55:28 -0300

    versão 3.1
    semana 3

--- python.py
+++ python.py
@@ -1,12 +1,16 @@
-nome = "maycon" # nome = um tipo de variavel
-idade = "16" # idade = um tipo de variavel
-print ("Ola,", nome,"idade", idade) # print = exibir informações ou a variavel
+nome = input("qual e a porra do seu nome: ")
+print (f"seu nome e {nome}")
+print("nome")
 
-notas = [2, 5, 6, 4] # nota = um tipo de variavel
+ano_nascimento = int(input("fala a porra do seu ano de nacimento seu lixo")) # int(input = serve para ler uma entrada de texto do usuário (console) e convertê-la imediatamente em um número inteiro
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+print(f"a sou idade e {idade} anos e agora eu sei onde vc mora.")
+
+notas = [999, 9, 99, 9] # nota = um tipo de variavel
 media = sum(notas) / len(notas) # sum = de somar elementos / len  é um método integrado utilizado para retornar o número de itens
 print(f"media é: {media:.2f}") # 2 = sumeros da soma / f = Permite que o Python interprete o que está dentro das chave
 
-numero = indut("digute o numero: ")
-numero_inteiro = int(numero)
-dobro = numero_iteiro * 2
-print("o dobro do numero digite é:", dobro)
+numero1 = input("digute a porra do numero") # input =  receber dados inseridos pelo usuário via teclado
+numero2 = int(numero1) # int = para representar e manipular números inteiros
+print(numero2*2)
