6d15efcf6d49f569c98ce490ea1ea77ac49bb43e
maycon <maynions@gmail.com>
2026-03-11 10:54:56 -0300

    versão 3
    semana 3

--- python.py
+++ python.py
@@ -6,7 +6,7 @@ notas = [2, 5, 6, 4] # nota = um tipo de variavel
 media = sum(notas) / len(notas) # sum = de somar elementos / len  é um método integrado utilizado para retornar o número de itens
 print(f"media é: {media:.2f}") # 2 = sumeros da soma / f = Permite que o Python interprete o que está dentro das chave
 
-numero = indut("digute a porra do numero: ")
+numero = indut("digute o numero: ")
 numero_inteiro = int(numero)
 dobro = numero_iteiro * 2
 print("o dobro do numero digite é:", dobro)
